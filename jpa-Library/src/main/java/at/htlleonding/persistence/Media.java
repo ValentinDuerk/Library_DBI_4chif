5079b88c0cf8806e20931af71b9474636c1f4705
ChristophLasinger <lasingerch@gmail.com>
2022-03-15 11:45:55 +0100

    add media entity

--- jpa-Library/src/main/java/at/htlleonding/persistence/Media.java
+++ jpa-Library/src/main/java/at/htlleonding/persistence/Media.java
@@ -1,13 +1,59 @@
 package at.htlleonding.persistence;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Media {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @ManyToMany()
+    private Set<Topic> topics = new HashSet<>();
+
+    @ManyToMany()
+    private Set<Author> authors = new HashSet<>();
+
+    @ManyToOne
+    private Genre genre;
+
+    @Column
+    private Date publicationDate;
+
+    @OneToMany
+    private Set<Publication> publication = new HashSet<>();
+
+    public Media() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Set<Topic> getTopics() {
+        return topics;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
 }
