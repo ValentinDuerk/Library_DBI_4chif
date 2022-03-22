9627e08cba555306d6d58519c45ffccaf5d975a8
ChristophLasinger <lasingerch@gmail.com>
2022-03-22 08:43:07 +0100

    add media types (and other stuff)

--- jpa-Library/src/test/java/at/htlleonding/persistence/LibraryRepositoryTest.java
+++ jpa-Library/src/test/java/at/htlleonding/persistence/LibraryRepositoryTest.java
@@ -1,14 +1,8 @@
 package at.htlleonding.persistence;
 
-import at.htlleonding.persistence.Author;
-import at.htlleonding.persistence.Book;
-import at.htlleonding.persistence.LibraryRepository;
-import io.quarkus.test.TestTransaction;
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.*;
 
 import javax.inject.Inject;
-import javax.transaction.Transactional;
 import java.time.LocalDate;
 
 @QuarkusTest
