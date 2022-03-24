2a7f1ede935f1329ebe06da8749db8c4184a8e2a
Wunder-WEBT <55655649+Wunder-WEBT@users.noreply.github.com>
2022-03-24 19:04:27 +0100

    Update AuditTrail.java

--- jpa-Library/src/main/java/at/htlleonding/persistence/AuditTrail.java
+++ jpa-Library/src/main/java/at/htlleonding/persistence/AuditTrail.java
@@ -8,9 +8,80 @@ public class AuditTrail {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(nullable = false)
     private String user;
+
+    @Column(nullable = false)
     private Date date;
+
+    @Column(nullable = false)
     private String table;
+
+    @Column(nullable = false)
     private Action action;
 
+    @Column
+    private Integer ContentId = null;
+
+    @Column
+    private String OldValue = null;
+
+    @Column(nullable = false)
+    private String NewValue;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTable() {
+        return table;
+    }
+
+    public void setTable(String table) {
+        this.table = table;
+    }
+
+    public Action getAction() {
+        return action;
+    }
+
+    public void setAction(Action action) {
+        this.action = action;
+    }
+
+    public Integer getContentId() {
+        return ContentId;
+    }
+
+    public void setContentId(Integer contentId) {
+        ContentId = contentId;
+    }
+
+    public String getOldValue() {
+        return OldValue;
+    }
+
+    public void setOldValue(String oldValue) {
+        OldValue = oldValue;
+    }
+
+    public String getNewValue() {
+        return NewValue;
+    }
+
+    public void setNewValue(String newValue) {
+        NewValue = newValue;
+    }
 }
