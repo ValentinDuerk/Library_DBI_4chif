c4abeafdff343706d6cb9f8b94f003a92381d494
KernPhilip <Philip_Kern@outlook.de>
2022-03-24 19:00:23 +0100

    Updated Customer

--- jpa-Library/src/main/java/at/htlleonding/persistence/Persons/Customer.java
+++ jpa-Library/src/main/java/at/htlleonding/persistence/Persons/Customer.java
@@ -1,4 +1,30 @@
 package at.htlleonding.persistence.Persons;
 
-public class Customer {
+import at.htlleonding.persistence.Person;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("Customer")
+public class Customer extends Person {
+    @Column
+    private boolean isEmployee;
+
+    public Customer(String firstName, String lastName, String eMail, String telNumber, boolean isEmployee) {
+        super(firstName, lastName, eMail, telNumber);
+        this.isEmployee = isEmployee;
+    }
+
+    public Customer() {
+    }
+
+    public boolean isEmployee() {
+        return isEmployee;
+    }
+
+    public void setEmployee(boolean employee) {
+        isEmployee = employee;
+    }
 }
